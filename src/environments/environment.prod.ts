f5267c46e0ce0de036869249a4a2f5d160db3afb
Abimbola Hassan <infinitizon@gmail.com>
2021-08-06 08:32:21 +0100

    Point to the test DB instead of the dollarfund DB

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   gateway: 'flutterwave',
-  apiUrl: 'https://dollarfund.azurewebsites.net'
+  apiUrl: 'https://chapel-hill-be.herokuapp.com'
+  // apiUrl: 'https://dollarfund.azurewebsites.net'
 };
