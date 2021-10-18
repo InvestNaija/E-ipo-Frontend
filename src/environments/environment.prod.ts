3143bec46854cce5475124a4e0c5c23287b8608f
InvestNaija <86833132+InvestNaija@users.noreply.github.com>
2021-10-18 15:54:44 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   gateway: 'flutterwave',
-  apiUrl: 'https://learningmicrosite.azurewebsites.net'
+  apiUrl: 'https://learningmicrosite.azurefd.net'
   // apiUrl: 'https://chapel-hill-be.herokuapp.com'
   // apiUrl: 'https://dollarfund.azurewebsites.net'
 };
