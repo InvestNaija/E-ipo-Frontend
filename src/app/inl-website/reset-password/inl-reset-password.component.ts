12cfae679d66edf321aa4ae86ef32280e671f35c
Abimbola Hassan <infinitizon@gmail.com>
2021-10-19 17:55:40 +0100

    add the regex to the reset password

--- src/app/inl-website/reset-password/inl-reset-password.component.ts
+++ src/app/inl-website/reset-password/inl-reset-password.component.ts
@@ -42,10 +42,11 @@ export class InlResetPasswordComponent implements OnInit {
       this.myForm = this.fb.group({
         password: [null, [
             Validators.required,
-            Validators.minLength(6),
+            Validators.minLength(6), Validators.maxLength(15),
             this.commonServices.regexValidator(new RegExp(this.commonServices.oneDigit), {'oneDigit': ''}),
             this.commonServices.regexValidator(new RegExp(this.commonServices.oneLowerCase), {'oneLowerCase': ''}),
             this.commonServices.regexValidator(new RegExp(this.commonServices.oneUpperCase), {'oneUpperCase': ''}),
+            this.commonServices.regexValidator(new RegExp(this.commonServices.specialChar), {'specialChar': ''}),
           ]
         ],
         confirmPassword: [null, Validators.required],
--- src/app/inl-website/reset-password/reset-password.validators.ts
+++ src/app/inl-website/reset-password/reset-password.validators.ts
@@ -2,9 +2,11 @@ export const ValidationMessages  = {
   'password' : {
     'required': 'Password is required',
     'minlength': 'Must be minimum of 6 characters',
+    'maxlength': 'Maximum of 15 characters',
     'oneDigit': 'Must contain one digit',
     'oneLowerCase': 'Must contain one lowercase letter',
     'oneUpperCase': 'Must contain one uppercase letter',
+    'specialChar': 'Must contain one special character e.g _, !, @, etc',
   },
   'confirmPassword' : {
     'required': 'Confirm Password is required',
